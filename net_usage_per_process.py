0e0d3f29aa169faa047d04a2ac6488e306a57498
Luca Porzio <94477476+Luca040619@users.noreply.github.com>
2022-11-10 18:46:07 +0100

    init 1.0

--- net_usage_per_process.py
+++ net_usage_per_process.py
@@ -1,3 +1,21 @@
+'''
+<Auto updater is a program which help you to update many apps installed on your pc,
+monitor the network usage for every process and shutdown the system when has finished>
+Copyright (C) 2022-present Luca Porzio
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+'''
 from scapy.all import *
 import psutil
 
